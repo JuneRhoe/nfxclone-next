109c3d1af2e3f135239fab77d496d8a67064bda2
JuneRhoe <junghyun.rhoe@gmail.com>
2025-04-29 10:33:51 -0400

    Update auth middleware

--- src/libs/middleware/mwDefinitions.ts
+++ src/libs/middleware/mwDefinitions.ts
@@ -1,7 +1,9 @@
 import { PATH_BROWSE } from '../definition-route'
 
-export const REGEX_IGNORE_PATHS =
-  /\/api\/|_next\/static|_next\/image|favicon.ico/i
-export const REGEX_PROTECTED_ROUTES = /^\/(browse|search)(\/[\w\-]*)*$/i
+// export const REGEX_IGNORE_PATHS =
+//   /\/api\/|_next\/static|_next\/image|favicon.ico/i
+export const REGEX_IGNORE_PATHS = /_next\/static|_next\/image|favicon.ico/i
+export const REGEX_PROTECTED_ROUTES =
+  /^\/(api\/user-info|api\/my-medias|api\/search|api\/morelike-medias|browse|search)(\/[\w\-]*)*$/i
 export const REGEX_SIGNED_IN_DEFAULT_ROUTE = /^\/(browse|about)(\/[\w\-]*)*$/i
 export const SIGNED_IN_DEFAULT_ROUTE = PATH_BROWSE
