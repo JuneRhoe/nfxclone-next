cda7bd46d306a431b46a8964aea3ce0f16c1ddf0
JuneRhoe <junghyun.rhoe@gmail.com>
2025-05-06 18:18:45 -0400

    Fix the modal animation

--- src/components/Browse/BrowseMediaContainer/MediaSliderContainer/components/MediaSlider/components/MediaSliderItemModal.tsx
+++ src/components/Browse/BrowseMediaContainer/MediaSliderContainer/components/MediaSlider/components/MediaSliderItemModal.tsx
@@ -97,65 +97,74 @@ export default function MediaSliderItemModal({
           transition: { default: { duration: 0.2, ease: 'easeInOut' } },
         }}
         style={{ cursor: isAnimationCompleted ? 'auto' : 'pointer' }}
+        onAnimationStart={() => setIsAnimationCompleted(false)}
         onAnimationComplete={() => setIsAnimationCompleted(true)}
       >
-        <div style={{ backgroundColor: `${COLOR_BACKGROUND}` }}>
-          <div className="h-full w-full">
-            <div className="h-[55%]" onPointerDown={closeModal}>
-              <Image
-                className="w-full rounded-sm"
-                src={getSliderItemTitleImg(mediaInfo.id)}
-                width="434"
-                height="250"
-                alt="Slider Item Image"
-              />
-            </div>
-            <div className="flex h-[45%] items-center justify-center">
-              <div className="flex h-full w-full flex-col gap-2 px-3 py-2 text-sm text-white">
-                <div className="flex h-full w-full items-center justify-between">
-                  <div className="flex items-center gap-2">
+        <div
+          className="h-full w-full rounded-md"
+          style={{ backgroundColor: `${COLOR_BACKGROUND}` }}
+        >
+          <Image
+            className="w-full rounded-sm"
+            src={getSliderItemTitleImg(mediaInfo.id)}
+            width="434"
+            height="250"
+            alt="Slider Item Image"
+            onPointerDown={closeModal}
+          />
+
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 1 } }}
+          >
+            {isAnimationCompleted && (
+              <div className="flex items-center justify-center">
+                <div className="flex h-full w-full flex-col gap-2 px-3 py-2 text-sm text-white">
+                  <div className="flex h-full w-full items-center justify-between">
+                    <div className="flex items-center gap-2">
+                      <IconButton
+                        icon={faPlay}
+                        buttonMode="secondary"
+                        onClick={() => {}}
+                      />
+                      <IconButton
+                        icon={isInMyList ? faCheck : faPlus}
+                        loading={isAddMyMediaLoading || isRemoveMyMediaLoading}
+                        onClick={() => {
+                          if (isInMyList) {
+                            removeMyMedia()
+                          } else {
+                            addMyMedia()
+                          }
+                        }}
+                      />
+                    </div>
+
                     <IconButton
-                      icon={faPlay}
-                      buttonMode="secondary"
-                      onClick={() => {}}
-                    />
-                    <IconButton
-                      icon={isInMyList ? faCheck : faPlus}
-                      loading={isAddMyMediaLoading || isRemoveMyMediaLoading}
-                      onClick={() => {
-                        if (isInMyList) {
-                          removeMyMedia()
-                        } else {
-                          addMyMedia()
-                        }
-                      }}
+                      icon={faAngleDown}
+                      onClick={() => onShowMoreInfoModal()}
                     />
                   </div>
 
-                  <IconButton
-                    icon={faAngleDown}
-                    onClick={() => onShowMoreInfoModal()}
-                  />
-                </div>
-
-                <div className="flex h-full w-full items-center gap-2 text-xs">
-                  <div
-                    className="flex items-center justify-center rounded-sm border-1 border-gray-400 px-1.5
-                      whitespace-nowrap"
-                  >
-                    {mediaInfo.ratingSymbol}
+                  <div className="flex h-full w-full items-center gap-2 text-xs">
+                    <div
+                      className="flex items-center justify-center rounded-sm border-1 border-gray-400 px-1.5
+                        whitespace-nowrap"
+                    >
+                      {mediaInfo.ratingSymbol}
+                    </div>
+                    <div className="flex items-center">
+                      {mediaInfo.ratingDetails?.slice(0, 2)?.join(' • ')}
+                    </div>
                   </div>
-                  <div className="flex items-center">
-                    {mediaInfo.ratingDetails?.slice(0, 2)?.join(' • ')}
-                  </div>
-                </div>
 
-                <div className="h-full w-full text-xs">
-                  {mediaInfo.genres?.slice(0, 2)?.join(' • ')}
+                  <div className="h-full w-full text-xs">
+                    {mediaInfo.genres?.slice(0, 2)?.join(' • ')}
+                  </div>
                 </div>
               </div>
-            </div>
-          </div>
+            )}
+          </motion.div>
         </div>
       </motion.div>
     </Modal>
