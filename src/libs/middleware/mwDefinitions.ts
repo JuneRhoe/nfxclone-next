6379aed654c28a4e43594c80bdae01cfab9dca7b
JuneRhoe <junghyun.rhoe@gmail.com>
2025-10-06 13:50:51 -0400

    Update local assets configuration

--- src/libs/middleware/mwDefinitions.ts
+++ src/libs/middleware/mwDefinitions.ts
@@ -2,7 +2,8 @@ import { PATH_BROWSE } from '../definition-route'
 
 // export const REGEX_IGNORE_PATHS =
 //   /\/api\/|_next\/static|_next\/image|favicon.ico/i
-export const REGEX_IGNORE_PATHS = /_next\/static|_next\/image|favicon.ico/i
+export const REGEX_IGNORE_PATHS =
+  /\/api\/|_next\/static|_next\/image|favicon\.ico|^\/assets\//i
 export const REGEX_PROTECTED_ROUTES =
   /^\/(api\/user-info|api\/my-medias|api\/search|api\/morelike-medias|browse|search)(\/[\w\-]*)*$/i
 export const REGEX_SIGNED_IN_DEFAULT_ROUTE = /^\/(browse|about)(\/[\w\-]*)*$/i
